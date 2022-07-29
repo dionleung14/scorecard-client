c66fdda16838f8bc18159ca907835c6d632febf4
Dion L <dioncleung@gmail.com>
2022-07-29 12:19:02 -0700

    fixed connection between client and server for post request body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,42 @@ import React from "react";
 
 export default function Home() {
   const defFunc = () => {
-    console.log("hey");
     fetch("/scorecards/home").then(async response => {
       console.log("maybe?");
-      await response.text();
+      let test = await response.text();
+      console.log(test);
+    });
+  };
 
+  const newFunc = () => {
+    fetch("/scorecards/test", {
+      method: "POST",
+      body: JSON.stringify({ key1: "value 1" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(async response => {
+      console.log("maybe?");
+      let test = await response.text();
+      console.log(test);
+    });
+  };
+
+  const getGamesInADay = () => {
+    fetch("sportradar/game/day", {
+      method: "POST",
+      body: JSON.stringify({
+        year: 2021,
+        month: 4,
+        day: 16,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(async response => {
+      console.log("possibly?");
+      let test = await response.text();
+      console.log(test);
     });
   };
 
@@ -16,6 +47,8 @@ export default function Home() {
       <div>
         <h3>hey</h3>
         <button onClick={defFunc}>Click me</button>
+        <button onClick={newFunc}>new Func</button>
+        <button onClick={getGamesInADay}>Games in a day</button>
       </div>
     </div>
   );
