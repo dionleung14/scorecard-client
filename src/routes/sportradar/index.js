fc41c0bedba0eb4a352eba86b8efe114074f75f8
Dion L <dioncleung@gmail.com>
2022-08-13 18:36:32 -0700

    removed unused routes

--- src/routes/sportradar/index.js
+++ src/routes/sportradar/index.js
@@ -3,17 +3,13 @@ import {
   getSingleGameBoxscore,
   getSingleGameFullInfo,
 } from "./game";
-import {
-  //getAllGamesInASeason,
-  getSeasonGamesForATeam,
-} from "./season";
+import { getSeasonGamesForATeam } from "./season";
 import { getStartingLineupForAGame, getPBPForAGame } from "./playbyplay";
 
 export {
   getGamesInADay,
   getSingleGameBoxscore,
   getSingleGameFullInfo,
-  // getAllGamesInASeason,
   getSeasonGamesForATeam,
   getStartingLineupForAGame,
   getPBPForAGame,
