8b633e14f858b2b3fa51667beefa66bb85f0e291
Dion L <dioncleung@gmail.com>
2022-12-26 16:54:32 -0800

    updated name of environment variable

--- netlify/functions/environment.js
+++ netlify/functions/environment.js
@@ -1,7 +1,7 @@
 exports.handler = async function (event, context) {
   console.log("we're inside the environments file wassuppppp");
   const value = process.env.REACT_APP_FAVORITE_POKEMON;
-  const newValue = process.env.SERVER_URL;
+  const newValue = process.env.REACT_APP_SERVER_URL;
 
   return {
     statusCode: 200,
