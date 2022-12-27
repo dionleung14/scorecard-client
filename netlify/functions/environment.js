da4c5f9c2acfbaa95d7f121328e95de9dba2474d
Dion L <dioncleung@gmail.com>
2022-12-26 16:48:23 -0800

    testing out netlify functions with environment variables

---
+++ netlify/functions/environment.js
@@ -0,0 +1,11 @@
+exports.handler = async function (event, context) {
+  console.log("we're inside the environments file wassuppppp");
+  const value = process.env.REACT_APP_FAVORITE_POKEMON;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `DIONNNNNNNNNNN : Value of MY_IMPORTANT_VARIABLE is ${value}.`,
+    }),
+  };
+};
