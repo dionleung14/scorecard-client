fe28fd4de9557dc59e21ea392400e566ef0f911d
Dion L <dioncleung@gmail.com>
2022-12-26 16:52:58 -0800

    adding server url?

--- netlify/functions/environment.js
+++ netlify/functions/environment.js
@@ -1,6 +1,7 @@
 exports.handler = async function (event, context) {
   console.log("we're inside the environments file wassuppppp");
   const value = process.env.REACT_APP_FAVORITE_POKEMON;
+  const newValue = process.env.SERVER_URL;
 
   return {
     statusCode: 200,
