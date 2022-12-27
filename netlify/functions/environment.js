8266718d2c35527db89a33c9494f70b66236918a
Dion L <dioncleung@gmail.com>
2022-12-26 16:57:01 -0800

    changing what's sent back in the environment file

--- netlify/functions/environment.js
+++ netlify/functions/environment.js
@@ -6,7 +6,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `DIONNNNNNNNNNN : Value of MY_IMPORTANT_VARIABLE is ${value}.`,
+      message: `DIONNNNNNNNNNN : Value of MY_IMPORTANT_VARIABLE is ${newValue}.`,
     }),
   };
 };
