4981fadcb165c6f0eae87ada3409ba1976374934
Dion L <dioncleung@gmail.com>
2022-12-26 16:37:04 -0800

    renaming environment variable

--- src/routes/sportradar/season.js
+++ src/routes/sportradar/season.js
@@ -3,7 +3,7 @@ export const getSeasonGamesForATeam = async searchTerms => {
   const { year, team, type: season } = searchTerms;
   const savedData = searchTerms.savedData || false;
   console.log("fetching games in a season for a team");
-  const pokemon = await process.env.FAVORITE_POKEMON;
+  const pokemon = await process.env.REACT_APP_FAVORITE_POKEMON;
   console.log(pokemon);
   let games = fetch(`sportradar/season/team`, {
     method: "POST",
