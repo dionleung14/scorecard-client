a13058c69ae00a1b67e6d5bf7010c03ec4aabc85
Dion L <dioncleung@gmail.com>
2023-01-13 18:57:18 -0800

    extracted pitcher to its own component for stateful lineup

--- src/pages/GameInfo/components/PitchingRecords.jsx
+++ src/pages/GameInfo/components/PitchingRecords.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
-export default function PitchingRecords() {
+export default function PitchingRecords({ pitcher }) {
+  const defensivePositionMapper = {
+    1: { positionName: "Pitcher", positionAbbr: "P" },
+    2: { positionName: "Catcher", positionAbbr: "C" },
+    3: { positionName: "First Baseman", positionAbbr: "1B" },
+    4: { positionName: "Second Baseman", positionAbbr: "2B" },
+    5: { positionName: "Third Baseman", positionAbbr: "3B" },
+    6: { positionName: "Shortstop", positionAbbr: "SS" },
+    7: { positionName: "Left Fielder", positionAbbr: "LF" },
+    8: { positionName: "Center Fielder", positionAbbr: "CF" },
+    9: { positionName: "Right Fielder", positionAbbr: "RF" },
+    10: { positionName: "Designated Hitter", positionAbbr: "DH" },
+    11: { positionName: "Pinch Hitter", positionAbbr: "PH" },
+    12: { positionName: "Pinch Runner", positionAbbr: "PR" },
+  };
+
   return (
-    <div>PitchingRecords</div>
-  )
+    <tr>
+      <td>{pitcher.jerseyNumber}</td>
+      <td>
+        {pitcher.firstName} {pitcher.lastName}
+      </td>
+      <td>{defensivePositionMapper[1].positionAbbr}</td>
+    </tr>
+  );
 }
--- src/pages/GameInfo/components/StatefulLineups.jsx
+++ src/pages/GameInfo/components/StatefulLineups.jsx
@@ -1,29 +1,23 @@
 import React, { useState } from "react";
 import { didPlayerGetSubbedOut } from "../../../services/evaluateLineupChange";
 import StatefulLineupRow from "./StatefulLineupRow";
-// import StatefulLineupRow from "./StatefulLineupRow";
+import PitchingRecords from "./PitchingRecords";
 
-export default function StatefulLineups({lineup: lineupProps, lineupChanges, team}) {
+export default function StatefulLineups({
+  lineup: lineupProps,
+  lineupChanges,
+  team,
+}) {
+  const [pitcher, setPitcher] = useState(lineupProps[0]);
   const [lineup, setLineup] = useState(lineupProps.slice(1, 10));
   if (!true) {
     // placeholder to "use" setLineup to avoid deployment bugs
     setLineup(true);
   }
-
-  const defensivePositionMapper = {
-    1: { positionName: "Pitcher", positionAbbr: "P" },
-    2: { positionName: "Catcher", positionAbbr: "C" },
-    3: { positionName: "First Baseman", positionAbbr: "1B" },
-    4: { positionName: "Second Baseman", positionAbbr: "2B" },
-    5: { positionName: "Third Baseman", positionAbbr: "3B" },
-    6: { positionName: "Shortstop", positionAbbr: "SS" },
-    7: { positionName: "Left Fielder", positionAbbr: "LF" },
-    8: { positionName: "Center Fielder", positionAbbr: "CF" },
-    9: { positionName: "Right Fielder", positionAbbr: "RF" },
-    10: { positionName: "Designated Hitter", positionAbbr: "DH" },
-    11: { positionName: "Pinch Hitter", positionAbbr: "PH" },
-    12: { positionName: "Pinch Runner", positionAbbr: "PR" },
-  };
+  if (!true) {
+    // placeholder to "use" setLineup to avoid deployment bugs
+    setPitcher(true);
+  }
 
   return (
     <div>
@@ -37,22 +31,19 @@ export default function StatefulLineups({lineup: lineupProps, lineupChanges, tea
           </tr>
           {lineup.length > 1
             ? lineup.map(player => {
-              let subbed = didPlayerGetSubbedOut(player, lineupChanges[lineupChanges.length - 1])
-              if (subbed) {
-                return <StatefulLineupRow player={player} removed={true}/>
-              } else {
-                return <StatefulLineupRow player={player}/>
-              }
+                let subbed = didPlayerGetSubbedOut(
+                  player,
+                  lineupChanges[lineupChanges.length - 1]
+                );
+                if (subbed) {
+                  return <StatefulLineupRow player={player} removed={true} />;
+                } else {
+                  return <StatefulLineupRow player={player} />;
+                }
               })
             : null}
           {/* Pitchers at the end at index 0 */}
-          <tr>
-            <td>{lineup[0].jerseyNumber}</td>
-            <td>
-              {lineup[0].firstName} {lineup[0].lastName}
-            </td>
-            <td>{defensivePositionMapper[1].positionAbbr}</td>
-          </tr>
+          <PitchingRecords pitcher={pitcher} />
         </tbody>
       </table>
     </div>
