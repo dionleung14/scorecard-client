b3e85e33c8c330d93300354ae541a8e4060d4955
Dion Leung <dioncleung@gmail.com>
2024-04-16 02:00:52 -0700

    refactoring search page

--- src/pages/SearchPastGames/SearchPastGames.jsx
+++ src/pages/SearchPastGames/SearchPastGames.jsx
@@ -1,12 +1,12 @@
 // This page retrieves past games in a season filtered by team
-// Using the back button after searching and clicking on a game clears the results
-// Open the game in a new tab?
 import React, { useState } from "react";
 import Game from "../../components/Game/Game";
 import GamesContainer from "../../components/GamesContainer/GamesContainer";
 import { getSeasonGamesForATeam } from "../../routes/sportradar";
 import teams from "../../data/teams";
 import SelectionYearsRange from "./components/SelectionYearsRange";
+import SearchForm from "./components/SearchForm";
+import { generateSearchResultsString } from "../../services/schedule/scheduleUtil";
 
 export default function SearchPastGames() {
   const [displayGames, setDisplayGames] = useState([]); // array to hold games from results
@@ -62,7 +62,29 @@ export default function SearchPastGames() {
         type: formAllGamesInSzn.type,
       });
       setSearchPending(false); // toggle search flag
-      generateSearchResultsString(formAllGamesInSzn);
+      setSearchString(generateSearchResultsString(formAllGamesInSzn));
+      // TODO: debug this so that all star game searches don't need the team
+      // } else if (
+      //   formAllGamesInSzn.year !== null &&
+      //   formAllGamesInSzn.type === "AST"
+      // ) {
+      //   setSearchPending(true); // toggle search flag
+      //   let games = await getSeasonGamesForATeam(formAllGamesInSzn); // fetch games
+      //   if (games !== null && games.length > 0) {
+      //     setDisplayGames(games); // displays games for a given team in a given year
+      //   } else {
+      //     setSearchPending(false); // toggle search flag
+      //     setErrorLoading(true);
+      //   }
+      //   setDisplaySearchTerms({
+      //     // set the display results graphic/text
+      //     ...displaySearchTerms,
+      //     year: formAllGamesInSzn.year,
+      //     team: formAllGamesInSzn.team,
+      //     type: formAllGamesInSzn.type,
+      //   });
+      //   setSearchPending(false); // toggle search flag
+      //   generateSearchResultsString(formAllGamesInSzn);
     } else {
       window.alert("Please fill out the form completely"); // alert user to fill out form
     }
@@ -77,66 +99,22 @@ export default function SearchPastGames() {
     });
   };
 
-  // generates string to display on search results page and sets in state
-  const generateSearchResultsString = formObj => {
-    const { year, team, type } = formObj;
-    let teamFinder = teams.filter(club => {
-      return club.abbr === team;
-    });
-    let seasonType;
-    if (type === "PRE") {
-      seasonType = "Preseason";
-    } else if (type === "PST") {
-      seasonType = "Postseason";
-    } else if (type === "REG") {
-      seasonType = "Regular Season";
-    }
-    let disp = `Search results for the ${year} ${teamFinder[0].market} ${teamFinder[0].name} ${seasonType}`;
-    setSearchString(disp);
-  };
+  // const searchForAllStarGame = () => {
+
+  // }
 
   return (
     <div>
       <h1>Search past games</h1>
-      <form onSubmit={submission} onReset={clearForm}>
-        <SelectionYearsRange
-          startYear="2016"
-          endYear="2023"
-          handleChange={handleChangeAllGamesInSzn}
-        />
-        <select
-          name="team"
-          id="team"
-          defaultValue="Choose a team"
-          onChange={handleChangeAllGamesInSzn}>
-          <option disabled>Choose a team</option>
-          {teams.map(team => {
-            return (
-              <option
-                key={team.abbr}
-                value={team.abbr}>{`${team.market} ${team.name}`}</option>
-            );
-          })}
-        </select>
-        <select
-          name="type"
-          id="type"
-          defaultValue="Choose season type"
-          onChange={handleChangeAllGamesInSzn}>
-          <option disabled>Choose season type</option>
-          <option value="PRE">Preseason</option>
-          <option value="REG">Regular season</option>
-          <option value="PST">Postseason</option>
-          <option value="AST">All-Star Game</option>
-        </select>
-        <input type="submit" value="Search" />
-        {formAllGamesInSzn.year ||
-        formAllGamesInSzn.team ||
-        formAllGamesInSzn.type ? (
-          <input type="reset" value="Reset form" />
-        ) : null}
-      </form>
-      {errorLoading ? <h3>Error: please contact dioncleung@gmail.com for more information</h3> : null}
+      <SearchForm
+        onSubmit={submission}
+        onReset={clearForm}
+        onChange={handleChangeAllGamesInSzn}
+        statefulForm={formAllGamesInSzn}
+      />
+      {errorLoading ? (
+        <h3>Error: please contact dioncleung@gmail.com for more information</h3>
+      ) : null}
       {searchPending ? (
         <h5>Searching</h5>
       ) : !searchPending && displayGames.length > 0 ? (
---
+++ src/pages/SearchPastGames/components/SearchForm.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import SelectionYearsRange from "./SelectionYearsRange";
+import teams from "../../../data/teams";
+
+export default function SearchForm({
+  onSubmit,
+  onReset,
+  onChange,
+  statefulForm,
+}) {
+
+  return (
+    <form onSubmit={onSubmit} onReset={onReset}>
+      <SelectionYearsRange
+        startYear="2016"
+        endYear="2024"
+        handleChange={onChange}
+      />
+      <select
+        name="team"
+        id="team"
+        defaultValue="Choose a team"
+        onChange={onChange}>
+        <option disabled>Choose a team</option>
+        {teams.map(team => {
+          return (
+            <option
+              key={team.abbr}
+              value={team.abbr}>{`${team.market} ${team.name}`}</option>
+          );
+        })}
+      </select>
+      <select
+        name="type"
+        id="type"
+        defaultValue="Choose season type"
+        onChange={onChange}>
+        <option disabled>Choose season type</option>
+        <option value="PRE">Preseason</option>
+        <option value="REG">Regular season</option>
+        <option value="PST">Postseason</option>
+        <option value="AST">All-Star Game</option>
+      </select>
+      <input type="submit" value="Search" />
+      {statefulForm.year ||
+      statefulForm.team ||
+      statefulForm.type ? (
+        <input type="reset" value="Reset form" />
+      ) : null}
+    </form>
+  );
+}
