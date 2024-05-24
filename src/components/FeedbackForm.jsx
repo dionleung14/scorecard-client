93e4770faf6b01833de9424a62b735819360cc6b
Dion Leung <dioncleung@gmail.com>
2024-05-23 22:44:21 -0700

    added a submission form lol

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function FeedbackForm() {
+  const [showDisplayForm, setShowDisplayForm] = useState(true);
+  const FEEDBACK_FORM_WRAPPER_ID = "feedback-form-wrapper";
+  const FEEDBACK_FORM_ID = "feedback-form";
+
+
+  const toggleShowDisplayForm = () => {
+    setShowDisplayForm(!showDisplayForm);
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    console.log("shup")
+  }
+
   return (
-    <span>Report an issue</span>
-  )
+    <div id={FEEDBACK_FORM_WRAPPER_ID} >
+      <p>
+        Report an issue{" "}
+        {!showDisplayForm ? (
+          <button onClick={toggleShowDisplayForm}>Show Form</button>
+        ) : (
+          <button onClick={toggleShowDisplayForm}>Hide Form</button>
+        )}
+      </p>
+      {showDisplayForm ? (
+        <div id={FEEDBACK_FORM_ID}>
+          <form onSubmit={submitForm}>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" />
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" />
+            <input type="submit" id="lname" name="lname" text="hi"/>
+          </form>
+        </div>
+      ) : null}
+    </div>
+  );
 }
