989703e72eb1435d9e812373c66be39b03d7488b
Dion Leung <dioncleung@gmail.com>
2024-07-02 01:34:46 -0700

    added batter out indicator to scorecards with random number of outs as placeholder reminder

--- src/pages/GameInfo/components/scorecards/ScoringCell5.jsx
+++ src/pages/GameInfo/components/scorecards/ScoringCell5.jsx
@@ -9,6 +9,7 @@ export default function ScoringCell5({ outcome, info }) {
   return (
     <td className="scorecard-cell scoring-cell" onClick={logInfo}>
       {outcome}
+      {info.isOut ? <p className="out-indicator">{info.outNumber}</p> : null}
     </td>
   );
 }
